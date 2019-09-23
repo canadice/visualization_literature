a3aea3dbbcfb0bc0ee40db2ca812f6b072e5953a
canadice <33020567+canadice@users.noreply.github.com>
2019-09-23 10:04:56 +0200

    Update to 0.6
    Added histogram and box plots in R

--- hietala_theme.R
+++ hietala_theme.R
@@ -2,6 +2,7 @@ require(ggplot2)
 hietala_theme <- theme_bw() +
   theme(axis.title.y = element_text(angle = 0, vjust = 0.5, size = 11),
         axis.title.x = element_text(size = 11),
+        plot.title = element_text(hjust = 0.5),
         panel.grid.major.x = element_blank(),
         panel.grid.minor.x = element_blank(),
         panel.grid.major.y = element_line(color = "gray70"),
@@ -9,4 +10,4 @@ hietala_theme <- theme_bw() +
         axis.text = element_text(size = 10, color = "black"))
 
 theme_set(hietala_theme)
-rm(hietala_theme)
+# rm(hietala_theme)
